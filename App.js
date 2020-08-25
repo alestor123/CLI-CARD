4150bea6ebad7e15a366bad90e0de0b0e1a7a955
alestor123 <alestoraldous@gmail.com>
2020-08-25 15:07:34 +0530

    added box mesr

--- App.js
+++ App.js
@@ -2,3 +2,14 @@
 // importing our modules
 var boxen = require('boxen'),
     chalk = require('chalk');
+
+// Because we like console, and captains.log sounds cool
+const captains = console;
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'single',
+};
+
