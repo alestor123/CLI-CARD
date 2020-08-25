adaa77d2f6ee85a3bfd5dd3222da855491995015
alestor123 <alestoraldous@gmail.com>
2020-08-25 15:11:35 +0530

    Added data

--- App.js
+++ App.js
@@ -13,3 +13,25 @@ const options = {
   borderStyle: 'single',
 };
 
+// Text + chalk definitions
+const data = {
+  name: chalk.white('Alestor Aldous /'),
+  handle: chalk.cyan('A Mortal Web Developer With Immortal Dreams'),
+  work: chalk.white('As A Full Stack Developer'),
+  github: chalk.cyan('https://github.com/alestor123'),
+  web: chalk.cyan('http://alestor123.github.io/'),
+  npx: chalk.white('npx alestor123-card'),
+  labelWork: chalk.white.bold('      Work:'),
+  labelGitHub: chalk.white.bold('    GitHub:'),
+  labelWeb: chalk.white.bold('       Web:'),
+  labelCard: chalk.white.bold('      Card:'),
+};
+
+// Actual strings we're going to output
+const newline = '\n';
+const heading = `${data.name} ${data.handle}`;
+const working = `${data.labelWork}  ${data.work}`;
+const githubing = `${data.labelGitHub}  ${data.github}`;
+const webing = `${data.labelWeb}  ${data.web}`;
+const carding = `${data.labelCard}  ${data.npx}`;
+
